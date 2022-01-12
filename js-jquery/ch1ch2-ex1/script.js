fbb378a96b21c5ae0f0afe26f3b22b120048a442
Lauren <rg326@live.com>
2022-01-12 18:59:48 -0500

    created files

---
+++ js-jquery/ch1ch2-ex1/script.js
@@ -0,0 +1,17 @@
+// Created on iPad.
+
+let today = new Date()
+let hourNow = today.getHours
+let greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good Evening'
+} else if (hourNow > 12) {
+    greeting = 'Good Afternoon'
+} else if (hourNow > 0) {
+    greeting = 'Good Morning'
+} else {
+    greeting = 'Welcome!'
+}
+
+document.write(greeting)

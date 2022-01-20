a8fe5dab5d3be59e5c673954470ecde8fb1d0738
Lauren <rg326@live.com>
2022-01-20 14:57:57 -0500

    added new lesson notes

---
+++ js-scrimba/1-counter/script.js
@@ -0,0 +1,87 @@
+//document.getElementById('count-el').innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+
+// let myAge = 31
+
+// console.log(myAge)
+
+// let myAge = 31
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+//let count = 5
+
+//count + 1
+//count = count + 1 // 5 + 1 = 6
+//count += 1
+//count++;
+
+// count = count + 2
+
+// count = count + 10
+
+// count = count - 2
+
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 100
+
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 25
+
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 70
+
+// console.log(bonusPoints)
+
+//  let count = document.getElementById('count-el')
+//  let increment = document.getElementById('increment-btn')
+
+// count = 0
+
+//  increment.addEventListener('click',()=>{
+//    count.innerText = count + 1;
+//  })
+
+//ACTUAL EXERCISE
+//initialise the count as 0
+//listen for clicks on the increment button
+//change the count-el in the HTML to reflect the new count
+
+// function increment(){
+//   console.log('The button was clicked')
+// }
+
+//==============
+
+// Setting up the the race 🏎 🏎 🏎
+
+
+function countDown(){
+  console.log(5)
+  console.log(4)
+  console.log(3)
+  console.log(2)
+  console.log(1)
+}
+countDown()
+
+
+
+// GO! 🏁
+// Players are running the race 🏎 💨
+// Race is finished! 🍾
+
+// Get ready for a new race 🏎 🏎 🏎
+
+countDown()
+
+
+//==============

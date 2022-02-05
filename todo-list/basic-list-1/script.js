4709d014d2876fafba159b81755af10ecaf2a87a
Lauren <rg326@live.com>
2022-02-05 15:41:53 -0500

    finished basic structure of todo list

--- todo-list/basic-list-1/script.js
+++ todo-list/basic-list-1/script.js
@@ -1,3 +1,30 @@
 let addBtn = document.getElementById('add-btn')
 let todoContainer = document.getElementById('todo-container')
-let 
+let  inputField = document.getElementById('field')
+
+//adds task
+addBtn.addEventListener('click',()=>{
+    let task = document.createElement('p')
+    let taskDiv = document.createElement('div')
+    task.classList.add('task-styling')
+    taskDiv.classList.add('taskDiv-styling')
+    task.textContent = inputField.value
+    todoContainer.appendChild(taskDiv)
+    taskDiv.appendChild(task)
+    inputField.value = ''
+
+    //adds line-through to finished task
+    task.addEventListener('click',()=>{
+        let taskBtn = document.createElement('button')
+        taskBtn.innerHTML = '-'
+        taskBtn.classList.add('btn-styling')
+        task.style.textDecoration = 'line-through'
+        task.appendChild(taskBtn)
+    })
+
+    //removes finished task
+    task.addEventListener('dblclick',()=>{
+    todoContainer.removeChild('taskDiv')
+    })
+
+})

e4f7e090648b1bd4390b5bd0a258a1e8f47e5709
Lauren <rg326@live.com>
2022-02-25 17:35:41 -0500

    edited folders and added notes for 10 days js day-1

--- 10-days-js/day-1/index.js
+++ 10-days-js/day-1/index.js
@@ -1,3 +1,20 @@
-//document.addEventListener('click',()=>{
-//    alert('thanks for clicking')
-//})
+let sum = 2 + 2
+
+console.log(sum)
+
+let myName;
+
+myName = 'Lauren'
+
+console.log(`My name is ${myName}.`)
+
+let strSum = '2' + '2'
+
+console.log(strSum)
+//'22'
+
+document.title = 'Hello'
+//changes title of page to 'Hello'
+
+document.body.style.backgroundColor = 'lightblue'
+//changes background color of document/page to light blue

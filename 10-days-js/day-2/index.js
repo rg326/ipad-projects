6e843499a83bd8df8618d68c5fef11da5a4ac86a
Lauren <rg326@live.com>
2022-02-26 17:33:30 -0500

    added notes for 10 days js’s day 2

--- 10-days-js/day-2/index.js
+++ 10-days-js/day-2/index.js
@@ -4,3 +4,15 @@
 
 //alert('bark')
 
+//function greet(x, y) {
+//    alert(`Hello, my name is ${x}. My favorite color is ${y}`)
+//}
+
+//greet('Lauren', 'blue')
+
+function tripleMe(x) {
+    return x * 5
+}
+
+let myFavNum = tripleMe(2)
+console.log(myFavNum)

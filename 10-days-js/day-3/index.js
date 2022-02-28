eaef2d6dc981cee90248b1c568c1eda93442d3b8
Lauren <rg326@live.com>
2022-02-27 20:45:54 -0500

    added day 3 to 10 days of js

---
+++ 10-days-js/day-3/index.js
@@ -0,0 +1,25 @@
+// // let catName = 'fluffy'
+// // let catAge = 4
+
+// // function meow() {
+// //     console.log('Meeeoww, my name is ' + catName)
+// // }
+
+// // meow()
+
+// let cat = {
+//     name: 'fluffy',
+//     age: 4,
+//     foods: {
+//         favorite: 'tuna',
+//         leastFavorite: 'oranges'
+//     },
+//     eyeColor: 'blue',
+//     meow() {
+//         alert('meowww')
+//     }
+// }
+
+// cat.age
+// cat.meow()
+// cats.foods.leastFavorite

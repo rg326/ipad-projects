5d1cd59bbb5fb22e30c02185275f2988a2ccac95
Lauren <rg326@live.com>
2022-03-01 10:40:45 -0500

    added day-5 to 10-days-js

---
+++ 10-days-js/day-5/index.js
@@ -0,0 +1,25 @@
+let strawberryCount = 20
+
+if(strawberryCount > 9){
+    console.log('Congrats!')
+} //else {
+//    console.log('Sorry, we do not ship orders that small.')
+//}
+
+//the else is an optional that works as an insurance plan
+
+let testVal = false
+
+//an empty string value evals to false, while a used one ex 'string', evals to true
+
+//console.log('There are 2 strawberries')
+
+let StrawberryCount = 2
+
+while(strawberryCount < 200){
+    console.log(`There are ${strawberryCount} strawberries.`)
+    strawberryCount = strawberryCount + 1
+    //strawberryCount += 1
+    //strawberryCount++
+}
+

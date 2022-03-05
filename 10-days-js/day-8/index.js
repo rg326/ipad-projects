891d92f2749dbaa3b0bc9c6be8c2c56915dc66a0
Lauren <rg326@live.com>
2022-03-05 17:09:42 -0500

    added context notes for day 8 in 10 days js

--- 10-days-js/day-8/index.js
+++ 10-days-js/day-8/index.js
@@ -1,7 +1,30 @@
-let myName = 'Lauren'
+// // let myName = 'Lauren'
 
-function myFunc(){
-    console.log(myName)
+// // function myFunc(){
+// // //let myName = 'Lauren the 2nd
+// //     if(2 + 2 == 4){
+// //     console.log(myName)
+// //     }
+// // }
+
+// // myFunc()
+
+// let john = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     driveCar(){
+//         function imAFuncNotAMethod(){
+//             console.log(this)
+//         }
+//         imAFuncNotAMethod()
+//         console.log(`${this.firstName} ${this.lastName} is driving a car.`)
+//     }
+// }
+
+// john.driveCar()
+
+function breathe(){
+    console.log(`${this.firstName} ${this.lastName} just inhaled and exhaled.`)
 }
 
-myFunc()
+breathe.call(john)

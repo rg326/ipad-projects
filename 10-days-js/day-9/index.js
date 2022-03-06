8ea337733941cd2c2888a1506b857ebcfce2b9af
Lauren <rg326@live.com>
2022-03-06 14:56:24 -0500

    added day 9 to 10 days js

---
+++ 10-days-js/day-9/index.js
@@ -0,0 +1,9 @@
+//ANONYMOUS FUNCTIONS
+
+document.addEventListener('click',function(){
+    alert('Thank you for clicking')
+})
+
+//ANONYMOUS FUNCTIONS
+
+document.addEventListener('click',() => alert('Thank you for clicking'))

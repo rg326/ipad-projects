08323656ecada072423335db38ea3afa3253b64e
Lauren <rg326@live.com>
2022-03-29 19:15:23 -0400

    brocode js lesson 2

---
+++ js-brocode/lesson-1/script.js
@@ -0,0 +1,14 @@
+console.log('I like pizza!')
+console.log('It\'s really good')
+
+window.alert('I REALLY LOVE PIZZA!')
+
+//This is a comment
+
+/*
+This
+is
+a
+multiline
+comment
+*/
---
+++ js-brocode/lesson-2/script.js
@@ -0,0 +1,18 @@
+//variable declaration
+// let age;
+
+// age = 21;
+
+// console.log(age)
+
+// //or 
+
+// let age = 21;
+
+//when accepting user input, it's better to do it in two steps
+
+let firstName = Lauren
+let age = 31
+
+console.log(firstName)
+console.log(age)

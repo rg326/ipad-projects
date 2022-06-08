0618474eb615da40b5b14b5b75ba0d8c01d93311
Lauren <rg326@live.com>
2022-06-07 20:12:33 -0400

    triple threat js initial commit

--- js-brocode/lesson-2/script.js
+++ js-brocode/lesson-2/script.js
@@ -11,8 +11,18 @@
 
 //when accepting user input, it's better to do it in two steps
 
-let firstName = Lauren
-let age = 31
+let firstName = 'Lauren'
+let age = '31' //str
+let student = true
 
-console.log(firstName)
-console.log(age)
+age = age + 1
+
+console.log('Hello', firstName)
+console.log('You are', age, 'years old')
+console.log('Enrolled', student)
+
+document.getElementById('p1').innerHTML = 'Hello ' + firstName
+
+document.getElementById('p2').innerHTML = 'You are', age, 'years old'
+
+document.getElementById('p3').innerHTML = 'Enrolled', student
---
+++ js-brocode/lesson-3/script.js
@@ -0,0 +1,31 @@
+let students = 22
+
+//numbers, expressions, etc are operands
+//signs are operators
+
+//students = students + 1
+//students = students - 1
+//students = students * 2
+//students = students / 2
+//let extraStudents = students % 2
+
+console.log(students)
+
+//students += 1 //augmented assignment operator
+//students -= 1
+//students *= 2
+//students /= 2
+//let extraStudents %= 3
+
+console.log(extraStudents)
+
+/* operator precedence
+	1. parenthesis()
+	2. exponents
+	3. multiplication & division
+	4. addition & subtraction
+*/
+
+let result = 1 + 2 * (3 + 4)
+
+console.log(result)
